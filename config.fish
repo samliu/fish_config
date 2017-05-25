d0f51c3f522fce4fd454d42b3d308d0a00bc8960
Sam Liu <sam@ambushnetworks.com>
2017-05-25 10:20:41 -0700

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,23 @@
+# Macports installs to /opt/local/bin so make it part of the path.
+set PATH /opt/local/bin $PATH
+
+# Enable usage of python binaries.
+set PATH /opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin $PATH
+
+# Which directory to use for fish.
+set -xU WORKON_HOME ~/.virtualenvs
+
+# Enable virtualfish; virtualenv for fish.
+eval (python -m virtualfish)
+
+# Activate our default virtualenv.
+vf activate default
+
+# Add virtualenv to fish prompt.
+functions -c fish_prompt _old_fish_prompt
+function fish_prompt
+  if set -q VIRTUAL_ENV
+    echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+  end
+  _old_fish_prompt
+end
